5a01c44bebe739ed2a43f27f4a60a2c2843b0af7
Manoj Samanta <samanta@homolog.us>
2019-09-28 05:19:54 +0000

    most of the logic done, but still struggling over one major bug

--- lib/typo_kart/lobby.ex
+++ lib/typo_kart/lobby.ex
@@ -1,18 +1,29 @@
 defmodule TypoKart.Lobby do
 
   # Players:
-  # 1. New players join and get game_id=0, when they are in the lobby.
-  # 2. When they join a game, game_id changes to something else.
-  #    The players can switch to different proposed games.
-  # 3. When >X players join a game, the game starts. The players 
-  #    cannot switch game after that point.
-  # 4. (Later: add player color selection)
+  # 1. New player joins and get game_id=0 (i.e. lobby)
+  #
+  # 2. When he clicks "join" on a game, game_id changes to something else.
+  #    The player can also switch to a different game unless the game started.
+  #
+  # 3. Game starts, when all open slots for the game is taken. The players cannot 
+  #    switch game after that point.
+  #
+  # 4. After game is over, player returns to lobby.
+  #
+  # 5. (maybe later: player can choose his color in game)
+  # 
+  # player state transition --> lobby -> game -> locked-game -> lobby
   #
   # Games:
-  # 1. We also keep track of running/initializing games.
-  # 2. At any time, keep three new games in the pipeline.
   #
-  # View:
+  # 1. We also keep track of initializing/running/ended games.
+  #
+  # 2. At any time, keep two new games in the pending pipeline.
+  #
+  # game state transition --> waiting --> play --> end
+  #
+  # LiveView/Web:
   # The view process displays players in the queue or playing, 
   # and games in the queue or in progress.
   #
@@ -20,11 +31,22 @@ defmodule TypoKart.Lobby do
   use GenServer
 
   def init(_init_arg) do
-    {:ok, %{games: %{}, players: %{}}  }
+    {:ok, %{
+       games: 
+        %{"game_1" => %{:status => :wait, "pos_1" => nil, "pos_2" => nil, "pos_3" => nil}, 
+          "game_2" => %{:status => :wait, "pos_1" => nil, "pos_2" => nil, "pos_3" => nil},
+          "game_3" => %{:status => :wait, "pos_1" => nil, "pos_2" => nil, "pos_3" => nil}
+        },
+          players: %{}
+      }
+    }
   end
 
-  def handle_cast({:join_lobby, player_id}, lobby) do
-    lobby=put_in(lobby, [:players, player_id], 0)
+  def handle_cast({:join_lobby, process_id}, lobby) do
+    id=UUID.uuid1()
+    player_id="player_" <> String.slice(id,0,3)
+    player_detail=%{player: player_id, time: System.os_time(:second), id: id, game: :lobby, pos: nil}
+    lobby=put_in(lobby, [:players, process_id], player_detail)
     {:noreply, lobby}
   end
 
@@ -32,9 +54,23 @@ defmodule TypoKart.Lobby do
   # 1. Locked players cannot change game
   # 2. When three players join, invoke "begin_game" and "lock players"
   #
-  def handle_cast({:join_game, player_id, game_id}, lobby) do
-    lobby=put_in(lobby, [:players, player_id], game_id)
-    {:noreply, lobby}
+  def handle_call({:join_game, player_id, game_id, pos}, _from, lobby) do
+    prev_game = lobby.players[player_id].game
+    prev_pos  = lobby.players[player_id].pos 
+
+    lobby =
+      case prev_pos do
+         nil -> lobby
+         _   -> put_in(lobby, [:games, prev_game, prev_pos], nil)
+    end
+
+    lobby=lobby |>
+          put_in([:players, player_id, :game], game_id) |>
+          put_in([:players, player_id, :pos], pos) |>
+          put_in([:games, game_id, pos], lobby.players[player_id].player)
+
+    #IO.inspect lobby
+    {:reply, lobby, lobby}
   end
 
 
@@ -63,24 +99,24 @@ defmodule TypoKart.Lobby do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def join_lobby(pid, player_id) do
-    GenServer.cast(pid, {:join_lobby, player_id})
+  def join_lobby(process_id) do
+    GenServer.cast(__MODULE__, {:join_lobby, process_id})
   end
 
-  def join_game(pid, player_id, game_id) do
-    GenServer.cast(pid, {:join_game, player_id, game_id})
+  def join_game(player_id, game_id, pos) do
+    GenServer.call(__MODULE__, {:join_game, player_id, game_id, pos})
   end
 
-  def list(pid) do
-    GenServer.call(pid, :list)
+  def list() do
+    GenServer.call(__MODULE__, :list)
   end
 
-  def list_players(pid) do
-    GenServer.call(pid, :list_players)
+  def list_players() do
+    GenServer.call(__MODULE__, :list_players)
   end
 
-  def list_games(pid) do
-    GenServer.call(pid, :list_games)
+  def list_games() do
+    GenServer.call(__MODULE__, :list_games)
   end
 
   defp create_game(game_id) do
@@ -88,12 +124,12 @@ defmodule TypoKart.Lobby do
   end
 
   defp start_game(game_id) do
-    # {:ok, pid} = Gamestate.start_link()
+    # {:ok} = Gamestate.start_link()
     {:ok}
   end
 
-  def game_ended(pid, game_id) do
-   GenServer.cast(pid, {:game_ended, game_id})
+  def game_ended(game_id) do
+   GenServer.cast(__MODULE__, {:game_ended, game_id})
   end
 
 end
